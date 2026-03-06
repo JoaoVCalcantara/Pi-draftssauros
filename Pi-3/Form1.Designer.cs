387302bb4ce75418ebf66dddc8c875649ee2af2c
João Vitor Costa Alcantara <136134745+JoaoVCalcantara@users.noreply.github.com>
2026-03-05 21:51:10 -0300

    Adicionar arquivos de projeto.

---
+++ Pi-3/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Pi_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Test = new System.Windows.Forms.Button();
+            this.txt_Test = new System.Windows.Forms.TextBox();
+            this.lb_Jurassicos = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_criar_partida = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Test
+            // 
+            this.btn_Test.Location = new System.Drawing.Point(12, 12);
+            this.btn_Test.Name = "btn_Test";
+            this.btn_Test.Size = new System.Drawing.Size(140, 33);
+            this.btn_Test.TabIndex = 0;
+            this.btn_Test.Text = "Ver partidas";
+            this.btn_Test.UseVisualStyleBackColor = true;
+            this.btn_Test.Click += new System.EventHandler(this.btn_Test_Click);
+            // 
+            // txt_Test
+            // 
+            this.txt_Test.Location = new System.Drawing.Point(12, 51);
+            this.txt_Test.Multiline = true;
+            this.txt_Test.Name = "txt_Test";
+            this.txt_Test.Size = new System.Drawing.Size(209, 316);
+            this.txt_Test.TabIndex = 1;
+            // 
+            // lb_Jurassicos
+            // 
+            this.lb_Jurassicos.AutoSize = true;
+            this.lb_Jurassicos.Location = new System.Drawing.Point(279, 405);
+            this.lb_Jurassicos.Name = "lb_Jurassicos";
+            this.lb_Jurassicos.Size = new System.Drawing.Size(56, 13);
+            this.lb_Jurassicos.TabIndex = 2;
+            this.lb_Jurassicos.Text = "Jurássicos";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(270, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(209, 316);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(501, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(501, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(501, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(564, 51);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(209, 316);
+            this.listBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(609, 405);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // btn_criar_partida
+            // 
+            this.btn_criar_partida.Location = new System.Drawing.Point(158, 12);
+            this.btn_criar_partida.Name = "btn_criar_partida";
+            this.btn_criar_partida.Size = new System.Drawing.Size(137, 33);
+            this.btn_criar_partida.TabIndex = 9;
+            this.btn_criar_partida.Text = "Criar partida";
+            this.btn_criar_partida.UseVisualStyleBackColor = true;
+            this.btn_criar_partida.Click += new System.EventHandler(this.btn_criar_partida_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_criar_partida);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lb_Jurassicos);
+            this.Controls.Add(this.txt_Test);
+            this.Controls.Add(this.btn_Test);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Test;
+        private System.Windows.Forms.TextBox txt_Test;
+        private System.Windows.Forms.Label lb_Jurassicos;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_criar_partida;
+    }
+}
+
---
+++ Pi-3/Form1.cs
@@ -0,0 +1,124 @@
+﻿using Draft;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Draft;
+
+namespace Pi_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label4.Text = Jogo.versao;
+            
+        }
+
+        private void btn_Test_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string retorno = Jogo.ListarPartidas("T");
+                txt_Test.Text = retorno;
+
+                retorno = retorno.Replace("\r"," ");
+                retorno = retorno.Substring(0, retorno.Length - 1);
+                string[] partidas = retorno.Split('\n');
+
+                listBox1.Items.Clear();
+                for (int i = 0; i< partidas.Length -1; i++)
+                {
+                    listBox1.Items.Add(partidas[i]);
+                }
+                
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string partida = listBox1.SelectedItem.ToString();
+                string[] dadosPartida = partida.Split(',');
+
+                int idPartida = Convert.ToInt32(dadosPartida[0]);
+                string nomePartida = dadosPartida[1];
+                string data = dadosPartida[2];
+
+                label1.Text = idPartida.ToString();
+                label2.Text = nomePartida;
+                label3.Text = data;
+
+                string retorno = Jogo.ListarJogadores(idPartida);
+                if (retorno.Substring(0, 4) == "ERRO")
+                {
+                    MessageBox.Show("Deu erro bixu, dont tem partidas disponiveis \n" + retorno, "PI 3", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+
+                    return;
+                }
+                else
+                {
+                    retorno = retorno.Replace("\r", "");
+                    string[] jogadore = retorno.Split('\n');
+
+                    listBox2.Items.Clear();
+                    for (int i = 0; i < jogadore.Length ; i++)
+                    {
+                        listBox2.Items.Add(jogadore[i]);
+                    }
+                }
+
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("PARA DE CLICAR VARIAS VEZES", "ANIMAL",MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                throw;
+
+            }
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_criar_partida_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                 string retorno = Jogo.CriarPartida("P", "2024-06-20", "Jurássicos");
+                string retorno2 = Jogo.Entrar(2018, "Joao123", "2024-06-20");
+                if (retorno.Contains("ERRO"))
+                {
+                    MessageBox.Show(retorno, "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                MessageBox.Show("Partida criada com sucesso, contendo o numero: " + retorno,"Pi 3", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
